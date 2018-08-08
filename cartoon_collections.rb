afc8967260d7b042a20ff80fb37982ed75db4a2f
Kevin K <doilerjar@outlook.com>
2018-08-08 00:24:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,19 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(array)
   # Your code here
+  i = 0 
+  result = []
+  while i < array.length 
+    result << yield(array[i])
+    i += 1 
+  end
+  binding.pry 
+  result2 = []
+  result.each_with_index do |item, index|
+    result2 << "#{index+1}. #{item}"
+  end
+  result2
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +28,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+input = ["Hi", "Bye", "Misty"]
+roll_call_dwarves(input) {|item| puts "#{item}"}
